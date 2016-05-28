9bee3cd82647f580b8c597c2ecc4c31b9840fb5f
Jason Brown <jasonbrown2k3@yahoo.com>
2016-05-27 19:50:45 -0500

    refactored call back to a promise

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var Client = require('node-rest-client').Client;
+var TTV = require('node-rest-client').Client;
+var Twitter = require('twitter');
 
-var client = new Client();
+const ttv = new TTV();
+const twitter = new Twitter();
 
 const members = {
     "YellowKillerBee": "@yellowkillerbee",
     "KungFuFruitCup": "@kungfufruitcup",
-    "AverageTrey" : "@AverageTrey"
+    "MissKaddyKins" : "@misskaddykins"
 };
 
 var args = {
@@ -14,11 +16,20 @@ var args = {
     }
 };
 
-Object.keys(members).forEach((m) => {
-    client.get(`https://api.twitch.tv/kraken/streams/${m}`, args, function (data, response) {
-    // parsed response body as js object
-    console.log(data);
-    // raw response
-    //console.log(response);
-    });
+Promise.all(Object.keys(members).map((m) =>{
+    return new Promise((ok) => {
+        ttv.get(`https://api.twitch.tv/kraken/streams/${m}`,args,(data) => {
+            ok(data.stream ? {
+                "Name": members[m],
+                "Preview": data.stream.preview.large
+            } : null);
+        })
+    })
+})).then((online) => {
+    var any = online.filter(on => !!on);
+    if (any.length) {
+        var twitters = any.reduce((o,k) => o + `${k.Name} `,'');
+        var links = any.reduce((o,k) => o + `${k.Preview} `,'');
+        console.log(`These Girls on Fire are live right now! ${twitters} ${links}`);
+    }
 });
